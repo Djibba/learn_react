d1cd6476037ba7f7f03e2a2a52d4f0297aa775bb
Djibril Ba <djibril15.ba@ucad.edu.sn>
2023-11-15 21:56:05 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Container from "./lessons/2.Premier-composant/Container"
+
+function App() {
+
+  return (
+    <div className="main-content">
+      <Container />
+    </div>
+  )
+}
+
+export default App
---
+++ src/lessons/1.Le-JSX/Container.jsx
@@ -0,0 +1,9 @@
+function Container(){
+    return (
+        <div className="container">
+            <p>Je suis dans un container</p>
+        </div>
+    )
+}
+
+export default Container
